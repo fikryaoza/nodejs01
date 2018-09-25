4b399628f50fd695bf3675f191fb6b3305544340
fikryaoza <43574461+fikryaoza@users.noreply.github.com>
2018-09-25 19:44:48 +0700

    First Commit

---
+++ config.js
@@ -0,0 +1,18 @@
+var config = {};
+
+config.port = 3000;
+
+//Authentication
+config.auth = false;
+
+//Database
+config.database = 'kasir';
+config.username = 'root';
+config.password = '';
+config.table_prefix = '';
+
+//Pagination
+config.paginate = true;
+config.page_limit = 2;
+
+module.exports = config;
